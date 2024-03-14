d3da3af3c7281da2b3e7393abc1ae90b7eb46fc6
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-14 19:06:33 -0300

    desafio 02: solução

---
+++ Desafio02/Program.cs
@@ -0,0 +1,11 @@
+﻿using Desafio02;
+
+Retangulo retangulo = new Retangulo();
+retangulo.Largura = 6;
+retangulo.Altura = 8;
+
+Console.WriteLine($"Características do retângulo: \n" +
+    $"\nLargura: {retangulo.Largura}\n" +
+    $"Altura: {retangulo.Altura}\n" +
+    $"Área: {retangulo.CalcularArea()}\n" +
+    $"Perímetro:  {retangulo.CalcularPerimetro()}");
---
+++ Desafio02/Retangulo.cs
@@ -0,0 +1,12 @@
+﻿namespace Desafio02
+{
+    public class Retangulo
+    {
+        public double Largura { get; set; }
+        public double Altura { get; set; }
+
+        public double CalcularArea() => Largura * Altura;
+
+        public double CalcularPerimetro() => 2 * Largura + 2 * Altura;
+    }
+}
